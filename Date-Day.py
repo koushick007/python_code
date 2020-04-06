4279923349d6c9145aeec1a5cf21b176a0840f8c
koushick007 <61386989+koushick007@users.noreply.github.com>
2020-04-06 21:50:47 +0530

    Python Practice

---
+++ Date-Day.py
@@ -0,0 +1,43 @@
+
+# Date to Day
+date=int(input("Enter the date : "))
+month=int(input("Enter the month : "))
+year=int(input("Enter the year : "))
+list_month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+list_weekdays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+list_end_date= [31,28,31,30,31,30,31,31,30,31,30,31]
+list_end_leap_date = [31,29,31,30,31,30,31,31,30,31,30,31]
+list_date = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+list_date2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+list_date_leap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+list_date4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+month_days = 0
+
+# Calculating year
+initial_year = year-1
+initial_month = 12
+initial_date = 31
+
+if date <= 31 and month <= 12:
+    cal_leap_year = initial_year / 4
+    normal_year = initial_year - cal_leap_year
+    year_in_days = (cal_leap_year * 366) + (normal_year * 365)
+
+    # Calculating months
+    if year % 4 == 0:
+        for i in range(0, month - 1):
+            month_days = month_days + list_end_leap_date[i]
+    else:
+        for i in range(0, month - 1):
+            month_days = month_days + list_end_date[i]
+
+    # Calculating days
+    final_days = month_days + date + year_in_days
+
+    # Calculating date to days
+    final = int(final_days % 7)
+    print(str(list_weekdays[final - 1]))
+else:
+    print("Wrong input! Please check your date and month again")
+
+
---
+++ Password_Generator.py
@@ -0,0 +1,43 @@
+import random
+maid_id = input("Enter the mail id - ")
+upper = int(input("Enter number of uppercase you want : "))
+lower = int(input("Enter number of lowercase you want : "))
+num = int(input("Total number you want ? "))
+any_special = int(input("Enter the special character you want in your password : "))
+total_len = upper + lower + num + any_special
+final_pass = ""
+
+upper_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+lower_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+sp_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', ':', ':', '"', '<', ',', '>', '.', '?', '/', '~', '`']
+num_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+if maid_id.find("@") !=-1:
+    for i in range(0, upper):
+        final_upper = random.choice(upper_list)
+        final_pass = final_pass + final_upper
+
+    for i in range(0, lower):
+        final_lower = random.choice(lower_list)
+        final_pass = final_pass + final_lower
+
+    for i in range(0, any_special):
+        final_special = random.choice(sp_list)
+        final_pass = final_pass + final_special
+
+    for i in range(0, num):
+        final_num = random.choice(num_list)
+        final_pass = final_pass + final_num
+
+    use_pass = final_pass
+    char_list = list(final_pass)
+    random.shuffle(char_list)
+    use_pass = ''.join(char_list)
+
+    print("Preferred Password - " + use_pass)
+else:
+    print("Incorrect mail id")
+
+
+
+
